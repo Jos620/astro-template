b0e9398f5cf193fd892c1b90983814cb84754bdb
Mateus Ito <mateusito10@gmail.com>
2023-12-08 08:15:11 -0300

    feat(lint): add prettier formatter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,11 +6,15 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
 };
