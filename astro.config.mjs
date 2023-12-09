cfcb37af46e539048dde67255d8cbe64ea73b7c8
Mateus Ito <mateusito10@gmail.com>
2023-12-09 15:24:47 -0300

    chore: lint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
